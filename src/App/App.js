cc0b22c8eacfbd423c6384474aa97b527a323f3b
Negarnbkhti42 <norob35@gmail.com>
2022-02-09 16:16:21 +0330

    navbar starter

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,19 @@ import CardList from '../components/CardList';
 import { list } from '../components/work_list';
 import logo from './undraw_to_do_re_jaef.svg';
 import './App.scss';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
-    list ? <CardList list={list} /> :
-    <div className='emptyList_logo'>
-      <img src={logo} alt='to do list' />
-    </div>
+    <>
+      <Navbar />
+      <main className='main'>
+        {list ? <CardList list={list} /> :
+          <div className='emptyList_logo'>
+            <img src={logo} alt='to do list' />
+          </div>}
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar_container">
+        <div className="navbar-lefHand">taskin</div>
+        <div className="navbar-rightHand">something else I'll put later</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
